6a5d2f6d39b3437f8fbaf9e8e32fdac4b40073d1
Peppe <abagnale.92@gmail.com>
2020-04-05 22:25:06 +0200

    full project

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNexteScene(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadFirsteScene(){
+        
+        SceneManager.LoadScene(0);
+    }
+
+    
+    public void Quit(){
+        
+        Application.Quit();
+    }
+}
